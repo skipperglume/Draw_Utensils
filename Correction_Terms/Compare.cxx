72153ca83ad1dd38e1fa7036d9c223e3007c81f6
GLUME <cdentimosh@gmail.com>
2022-11-02 14:05:28 +0100

    rename folder

--- Correction_Terms/Compare.cxx
+++ Correction_Terms/Compare.cxx
@@ -38,12 +38,52 @@ vector<double>FSTDV(string str){
 }
 
 
+TLegend * Custom_Legend_Coordinates(std::vector<float> coordinates){
+    if (coordinates.size() !=4 ){
+        std::cout<< "Wrong size of coordinates vector\n";
+        return NULL;
+    }
+
+    TLegend * lg = new TLegend(coordinates[0],coordinates[1],coordinates[2],coordinates[3]);
+    return lg;
+}
+
 void Compare(){
-    Term_Name = "NPV";
+    Term_Name = "Mu";
+
+
+    if (Term_Name != "NPV" && Term_Name != "Mu"){
+        cout<< "Error of term name\n";
+        return;
+    }
     vector<double> Term1;
     vector<double> Term2;
-    Term1 = FSTDV("        -0.001    -0.009          -0.041 -0.006 -0.059 -0.074 -0.284 0.095 -0.033           -0.194 -0.044 0.029");
-    Term2 = MuTerm;
+    // NPV
+    //   0.027 0.002 0.086 -0.035 0.073 0.079 0.398 -0.212 0.008 0.340 0.086 0.026
+    // Mu
+    //  -0.001 -0.009 -0.041 -0.006 -0.059 -0.074 -0.284 0.095 -0.033 -0.194 -0.044 0.029
+
+    
+    
+    if (Term_Name == "Mu" ){
+        // UFO MC20a
+        // Term1 = FSTDV(" -0.017 -0.011 -0.039 -0.012 -0.055 -0.087 -0.316 0.122 -0.057 -0.229 -0.012 0.225");
+        // Term2 = FSTDV(" -0.006 -0.011 -0.037 -0.016 -0.056 -0.086 -0.318 0.122 -0.064 -0.224 -0.011 0.224");
+        Term1 = FSTDV(" -0.014 -0.009 -0.033 -0.009 -0.061 -0.081 -0.315 0.134 -0.068 -0.218 -0.022 0.234");
+        Term2 = FSTDV(" -0.002 -0.009 -0.036 -0.007 -0.063 -0.082 -0.316 0.136 -0.071 -0.216 -0.018 0.232");
+        
+        
+    }
+        
+    if (Term_Name == "NPV" ){
+        // UFO MC20a
+        // Term1 = FSTDV(" 0.016 0.012 0.090 -0.060 -0.018 0.086 0.598 -0.227 0.046 0.371 0.063 -0.220");
+        // Term2 = FSTDV(" 0.027 0.010 0.093 -0.059 -0.016 0.085 0.603 -0.228 0.061 0.368 0.058 -0.222");
+        Term1 = FSTDV(" 0.020 0.010 0.117 -0.059 -0.017 0.072 0.572 -0.236 0.055 0.357 0.076 -0.250");
+        Term2 = FSTDV("0.029 0.008 0.121 -0.060 -0.010 0.070 0.583 -0.239 0.061 0.358 0.070 -0.249");
+    }
+        
+    
     gROOT->SetStyle("ATLAS");
     TGraph * Term_Graph1 = new TGraph(ResidualAbsEtaBins.size());
     TGraph * Term_Graph2 = new TGraph(ResidualAbsEtaBins.size());
@@ -86,26 +126,39 @@ void Compare(){
     
     
     TMultiGraph *mg = new TMultiGraph();
-    mg->SetTitle("1D corr params: a vs ade;#eta;#frac{#partial p_{T}}{#partial #mu}");
-    //mg->SetTitle("1D corr params: a vs ade;#eta;#frac{#partial p_{T}}{#partial NPV}");
+    if (Term_Name == "Mu" )
+        mg->SetTitle("1D corr params: a vs ade;#eta;#frac{#partial p_{T}}{#partial #mu}");
+    if (Term_Name == "NPV" )
+        mg->SetTitle("1D corr params: a vs ade;#eta;#frac{#partial p_{T}}{#partial NPV}");
     mg->Add(Term_Graph1,"APL");
     mg->Add(Term_Graph2,"APL");
     mg->Draw("a");
 
-    TLegend *  legend = new TLegend(0.5,0.7,0.9,0.9);
+    std::vector<float> MuLegCoord  =    {0.5,0.7,0.9,0.9};
+    std::vector<float> NPVLegCoord =    {0.5,0.2,0.9,0.4};
+
+    TLegend *  legend ;
+    if (Term_Name == "Mu" )
+        legend = Custom_Legend_Coordinates(MuLegCoord);
+    if (Term_Name == "NPV" )
+        legend = Custom_Legend_Coordinates(NPVLegCoord);
+
 
 
        
     
-    legend->AddEntry(Term_Graph1,"mc16 all","lp");
-    legend->AddEntry(Term_Graph2,"mc16a","lp");
+    legend->AddEntry(Term_Graph1,"MC20a UFO NoArea","lp");
+    legend->AddEntry(Term_Graph2,"MC20a UFO Area","lp");
     legend->Draw();
 
     char * name_to_save = From_String_To_Char_Array("./"+Term_Name+"Term"+".png");
     cout<<name_to_save<<endl;
     c->SaveAs(name_to_save);
-    
+    cout<<"All mc16 params: \n";
+    Print_Vector_Line(Term1);
+    cout<<"mc16a params: \n";
+    Print_Vector_Line(Term2);
     
     
     return;
-}
+}   
